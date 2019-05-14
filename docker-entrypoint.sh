0be4ccf523dde05028ebd190e3498077c0ff82ba
Nils Weller <nweller@uni-bremen.de>
2019-05-15 01:42:35 +0200

    Add docker config settings override

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 umask 0000 # avoid file deletion problems on host due to owner being root
+
+# Append existing environment variables to config, thereby enabling settings overrides.
+# TODO Add SOLTIX_ prefix for soltix-related settings variables, then filter?
+set >>./settings.cfg.sh
+
 exec "$@"
