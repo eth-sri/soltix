d1b9cec09f4995d85f92581c86d09a20c7c954b8
Nils Weller <nweller@uni-bremen.de>
2019-09-23 04:45:05 +0200

    First correctly interpreted AS test case

---
+++ test-interpretation/tests/7_first_working_AS/c0.sol
@@ -0,0 +1,201 @@
+contract c0 {
+struct s0 {
+uint248 m0;
+int240 m1;
+bool m2;
+}
+s0 v0 = s0(uint248(101), int240(-74), true);
+struct s2 {
+uint224 m0;
+bool m1;
+int112 m2;
+string m3;
+int152 m4;
+}
+struct s5 {
+bytes21 m0;
+int88 m1;
+bool m2;
+bytes30 m3;
+int48 m4;
+}
+struct s7 {
+int112 m0;
+}
+struct s6 {
+s7 m0;
+}
+struct s4 {
+s5 m0;
+uint200 m1;
+s6 m2;
+bytes24 m3;
+}
+struct s3 {
+uint184 m0;
+int88 m1;
+uint96 m2;
+bytes6 m3;
+s4 m4;
+}
+struct s1 {
+bytes9 m0;
+s2 m1;
+s3 m2;
+}
+s1 v1 = s1(bytes9(0x5DC352837A210871B0), s2(uint224(198), false, int112(254), "r\u0021\u003c\u00a7\u0092\u008e\u0040\u00eb\u00b9\u00c6\u003e\u002dq\u00d4\u0099\u00a3M\u0011\u00efD\u003aN\u005f\u00db\u00cecD\u0008\u00ab\u0013\u009b\u002e\u0008\u00b5G\u001e\u00b0Y\u003a\u002b\u0098\u00cf\u0083Y\u000e\u009b\u00ae\u0095\u005e\u00f1\u00af9q\u00b4\u0008\u00a8Q\u00d0\u0028tR", int152(80)), s3(uint184(24), int88(7357695493995382949792282), uint96(191), bytes6(0x611BC335F107), s4(s5(bytes21(0xBEA2EF18F5D137D541A3B7CC761F82BD0F8F5EE1E0), int88(-70389921608030723277130304), true, bytes30(0xB577CD157AF53D747B111CE4DCCC5921E36E21AB848DEA612C227ADF6987), int48(70201473435548)), uint200(136658431993835462798865419940781487193011800804459426378345), s6(s7(int112(-80))), bytes24(0xE3B8B885F7BFBDD98844BEB11184CF87B887A0B65BED4364))));
+string v2 = "\u007d\u00f7\u00ad\u00c4\u0019k\u00df\u00c6\u0080S\u0002\u003d\u005c\u0023N\u009b\u003dx\u0015\u0089\u00cd\u00d9\u0018\u0090\u00f2FVz\u00db\u0085\u00b8\u000bG\u00c62\u0097\u002a\u009c\u007c\u00eax\u00c4\u00d1\u0084\u0026q\u0005\u00f5\u0010\u001e\u00e5\u009d\u007d\u00d92y\u0092I\u0010\u0017m6\u00d6\u0000\u00bf\u00ae\u00f5\u000b\u002d\u0021\u00b8\u002eU\u00d22C\u0011X\u00c24s";
+bytes17 v3 = bytes17(0xB306A36F6302E814E491D1D1C9C7EA9ACA);
+bytes3 v4 = bytes3(0xBEF313);
+bool v5 = true;
+struct s10 {
+bool m0;
+bool m1;
+}
+struct s9 {
+bytes16 m0;
+s10 m1;
+}
+struct s11 {
+string m0;
+int232 m1;
+bytes17 m2;
+uint112 m3;
+bool m4;
+}
+struct s8 {
+int48 m0;
+int64 m1;
+s9 m2;
+s11 m3;
+}
+s8 v6 = s8(int48(-136349437157721), int64(-7061255749655530310), s9(bytes16(0x31B5298009590958540FA345D5025322), s10(false, true)), s11("\u00cfAO\u00d8\u000d\u00e6r\u00ca\u0081\u0020\u0081\u00ab\u001f\u00f5\u0080\u001cz\u00fbe7\u008f\u009d\u00bc\u001e\u00d5\u000f\u00e8g\u0016\u00aa\u00d7\u00dc\u00b1\u00de\u00a2\u002fv\u0013L\u009b\u001f\u00ab6\u0000\u00e0\u00c1Z\u00915e\u00a6\u0029\u0023\u00dd\u000c2\u00eb\u00e9\u00fe\u003d\u00d8\u0009\u0024\u00c5s\u00a5\u00dd\u00dd\u00f3\u003fEX\u0026\u00f3\u00d5\u009bdN\u00c9A\u005b\u00c1\u00a9\u00c9\u00bb\u0028\u0007\u00c8\u00a5\u0060\u00d5k\u00fdn\u00fa\u00b7P\u0084\u00f7\u009f\u0009\u0004\u00a4\u00cd\u0023\u00d7\u00ed3K\u007fya\u00bb\u00ac\u00d97\u00a4\u00d1\u00c5\u00ba\u000f\u00a4V\u00c9\u00afz\u00e2\u00e3\u00cef\u0083r\u00d9\u000f\u008f\u00f3\u00d6\u00b1\u00c4\u00af\u00ec\u0001\u00d7\u0021\u00delL\u00aa\u000c\u00e5\u005f\u0019\u0003t\u00d1\u0024\u003f\u00a0\u00eea\u00ef", int232(-458761576291014925711181562372233782866408670876323080641323827807621), bytes17(0xAD0404B0BEBBACA0EB15FE02AB26D47D7F), uint112(23), true));
+struct s13 {
+int120 m0;
+uint232 m1;
+}
+struct s12 {
+string m0;
+s13 m1;
+bytes22 m2;
+}
+s12 v7 = s12("\u002b\u00fe\u003e\u00f7\u00f1\u003a\u00e4\u00acZ\u0096\u00efBk\u00e1\u0084q\u0089\u00dc\u00b2\u00e1B\u000f\u000a\u0029\u001dH\u003c\u00d5\u009e\u00124\u00e8\u003f\u005eN\u0011P\u00b3\u0015\u0086\u001cl\u00dd\u00a1\u00ac\u001dJ\u0021\u00c7\u00a6r\u009a\u008b", s13(int120(-201), uint232(6334138768497698119073787099147080978219470757730483262463182567895884)), bytes22(0xEDA459C066C2D371C696949CCF6AEF7C404B0236F54A));
+bytes15 v8 = bytes15(0xCBF278CD86208E141717016A8D21D6);
+int216 v9 = int216(-244);
+event EXPR_ERROR(uint arg);
+function f0(int16 a0, int136 a1, bytes11 a2, bytes3 a3) public payable
+{
+v7.m1.m1 = //1
+(((!(v0.m2 != (((uint240(uint256(bytes32(a2))) % uint240(2)) == uint240(1))? true: false))) == (((v6.m1 % int64(2)) == int64(1))? true: false))? uint232(0): uint232(2205442755635794394898632573590888937455997949100563802462809736396192));
+if (v7.m1.m1 != uint232(0)) emit EXPR_ERROR(0);
+v8 = //2
+(((keccak256(bytes(v2)) & keccak256(bytes((((~(~a3)) == bytes3(0x321E8F))? "\u00d4\u001a\u0029\u0021Fd\u00bd\u00ceD\u00e9\u00b7\u0092\u00e5ZT\u001cMf6\u001a\u00ddm\u00aa\u0060T\u00b53\u00ca\u0023\u00c9\u00cd\u0060\u00cf\u0005\u00aaW\u00efA\u00b4\u00b3\u00ad\u0040\u005b\u008dF\u0099\u008b\u009d\u0011\u0013\u00943\u0021\u00ad\u0027\u0095F\u001f\u0014\u00c3\u00ae\u0025\u00cb\u0096\u00ea\u00e7\u00da\u009dA\u00a5\u00d1g\u00ae\u00e2\u000d\u008a\u00f0\u00c6\u00dap\u0019\u009fA\u00d3\u000b\u002b\u009d\u00e0\u003f\u0007\u00d22\u00c7\u007e\u007f\u001ae\u0014\u0022S\u00ebJy\u00be\u00dc\u0092\u00cf\u00c5K\u00d8\u0006\u00d0\u00b2\u00c0\u005f\u0087N\u0097\u001a\u00be\u00eb\u00e1\u00cc\u0084\u00f0\u00afYCr\u0018\u0008\u00b9\u00e7\u00c8\u0011\u0082\u00e7\u0021Z\u00df\u00cd0\u00af\u00cc\u00fd\u00be\u002c\u00b8\u0094\u00cd\u00b4\u00d0\u0019\u00ef\u00b3s\u009c\u00adVUK\u003cr\u00c0\u0027\u00fda": "\u00f5\u00b8\u009f\u007b\u0009\u00c8\u0005I\u0098\u008f\u00d6\u001d\u00d0\u00d2\u008f\u00b7\u00a8\u0029\u00b3E\u00c6ZS\u002bm\u003a\u0023\u0010\u0080\u00a9\u003a\u00bb8e\u009f\u001d\u00e1\u00af\u00b7L\u0085\u002e\u009ee\u00cc\u0018\u0020\u0024\u00d0\u005f\u00dfB\u00dc\u0095\u00a2\u0017\u00264lY\u00e2NV\u00a6\u00c1\u00ddmF\u0011\u00ac\u002c\u007fP4\u0029\u0027\u00f7\u00fc\u00ae\u002d\u008b\u00af\u00ddz\u00d3\u008e\u00d8\u0009Z\u00f22\u00e3\u00c2U\u007b\u0097\u00a2\u008d\u007c\u00cf\u00e2\u003a\u00f0\u00f1u\u00b6\u0026\u000cw\u008c\u001b\u00e7Q\u0027\u00d2\u00bb\u008fT\u00e8\u00f3\u008c\u005c\u0022W\u00c9o\u00e2\u003c\u003b\u00dd\u002d\u008f7\u00b1m\u0024\u000fY\u0023\u0040u\u0083\u00dc7\u000e\u00ac\u008c\u00d6\u007c\u00aapt\u0022\u002b\u002f\u00cd\u00c65\u00d65\u00d1\u00bd\u0095\u002c\u00c0\u00c1")))) < bytes32(int256((int216(a1) / (++v9)))))? bytes15(0x4CE85C2BEB1370154DB3EEEA4F1CD8): bytes15(0x099BCE706D8C41811B73C8A1C89316));
+if (v8 != bytes15(0x099BCE706D8C41811B73C8A1C89316)) emit EXPR_ERROR(1);
+v3 = //5
+bytes17(((~__returnFunction1((--a0))) ^ ((!((~(~(__returnFunction0((~v3), a2, a1) | bytes7(bytes32(int256(v6.m1)))))) <= bytes7(keccak256(bytes(v2)))))? bytes26(0x093943E7D9E57677A0C431ACF6AE23AEE0400E3F4843E295009C): bytes26(0xD995B8ABC7BC4F193B73F357160CD90FE31687BAA8DC6C77B149))));
+if (v3 != bytes17(0xF6C6BC18261A89885F3BCE530951DC511F)) emit EXPR_ERROR(2);
+v3 = //7
+(((bytes3(v3) >= (bytes3(a3) ^ (~v4))) || (((__returnFunction2((v5 || (((a1 % int136(2)) == int136(1))? true: false)), (~(a0++))) % int192(2)) == int192(1))? true: false))? bytes17(0x7D70623A28DFC9177D845320BE965A5EF3): bytes17(0xB056EEE23A9639A42F006D664D6BD368C2));
+if (v3 != bytes17(0x7D70623A28DFC9177D845320BE965A5EF3)) emit EXPR_ERROR(3);
+v4 = //8
+bytes3(v4);
+if (v4 != bytes3(0xBEF313)) emit EXPR_ERROR(4);
+}
+function __returnFunction0(bytes17 arg0, bytes11 arg1, int136 arg2) public returns (bytes7) {return //3
+((bytes17((arg1 | bytes11(bytes32(int256(arg2))))) > arg0)? bytes7(0x12C8D843568956): bytes7(0x72455866096607)); }
+function __returnFunction1(int16 arg0) public returns (bytes26) {return //4
+bytes26(bytes32(int256(arg0))); }
+function __returnFunction2(bool arg0, int16 arg1) public returns (int192) {return //6
+int192((arg1 + (arg0? int16(192): int16(0)))); }
+event finalResult_v0_m0(uint248 v0_m0);
+event finalResult_v0_m1(int240 v0_m1);
+event finalResult_v0_m2(bool v0_m2);
+event finalResult_v1_m0(bytes9 v1_m0);
+event finalResult_v1_m1_m0(uint224 v1_m1_m0);
+event finalResult_v1_m1_m1(bool v1_m1_m1);
+event finalResult_v1_m1_m2(int112 v1_m1_m2);
+event finalResult_v1_m1_m3(string v1_m1_m3);
+event finalResult_v1_m1_m4(int152 v1_m1_m4);
+event finalResult_v1_m2_m0(uint184 v1_m2_m0);
+event finalResult_v1_m2_m1(int88 v1_m2_m1);
+event finalResult_v1_m2_m2(uint96 v1_m2_m2);
+event finalResult_v1_m2_m3(bytes6 v1_m2_m3);
+event finalResult_v1_m2_m4_m0_m0(bytes21 v1_m2_m4_m0_m0);
+event finalResult_v1_m2_m4_m0_m1(int88 v1_m2_m4_m0_m1);
+event finalResult_v1_m2_m4_m0_m2(bool v1_m2_m4_m0_m2);
+event finalResult_v1_m2_m4_m0_m3(bytes30 v1_m2_m4_m0_m3);
+event finalResult_v1_m2_m4_m0_m4(int48 v1_m2_m4_m0_m4);
+event finalResult_v1_m2_m4_m1(uint200 v1_m2_m4_m1);
+event finalResult_v1_m2_m4_m2_m0_m0(int112 v1_m2_m4_m2_m0_m0);
+event finalResult_v1_m2_m4_m3(bytes24 v1_m2_m4_m3);
+event finalResult_v2(string v2);
+event finalResult_v3(bytes17 v3);
+event finalResult_v4(bytes3 v4);
+event finalResult_v5(bool v5);
+event finalResult_v6_m0(int48 v6_m0);
+event finalResult_v6_m1(int64 v6_m1);
+event finalResult_v6_m2_m0(bytes16 v6_m2_m0);
+event finalResult_v6_m2_m1_m0(bool v6_m2_m1_m0);
+event finalResult_v6_m2_m1_m1(bool v6_m2_m1_m1);
+event finalResult_v6_m3_m0(string v6_m3_m0);
+event finalResult_v6_m3_m1(int232 v6_m3_m1);
+event finalResult_v6_m3_m2(bytes17 v6_m3_m2);
+event finalResult_v6_m3_m3(uint112 v6_m3_m3);
+event finalResult_v6_m3_m4(bool v6_m3_m4);
+event finalResult_v7_m0(string v7_m0);
+event finalResult_v7_m1_m0(int120 v7_m1_m0);
+event finalResult_v7_m1_m1(uint232 v7_m1_m1);
+event finalResult_v7_m2(bytes22 v7_m2);
+event finalResult_v8(bytes15 v8);
+event finalResult_v9(int216 v9);
+function __outro() public payable
+{
+uint __loopCounter;
+emit finalResult_v0_m0(v0.m0);
+emit finalResult_v0_m1(v0.m1);
+emit finalResult_v0_m2(v0.m2);
+emit finalResult_v1_m0(v1.m0);
+emit finalResult_v1_m1_m0(v1.m1.m0);
+emit finalResult_v1_m1_m1(v1.m1.m1);
+emit finalResult_v1_m1_m2(v1.m1.m2);
+emit finalResult_v1_m1_m3(v1.m1.m3);
+emit finalResult_v1_m1_m4(v1.m1.m4);
+emit finalResult_v1_m2_m0(v1.m2.m0);
+emit finalResult_v1_m2_m1(v1.m2.m1);
+emit finalResult_v1_m2_m2(v1.m2.m2);
+emit finalResult_v1_m2_m3(v1.m2.m3);
+emit finalResult_v1_m2_m4_m0_m0(v1.m2.m4.m0.m0);
+emit finalResult_v1_m2_m4_m0_m1(v1.m2.m4.m0.m1);
+emit finalResult_v1_m2_m4_m0_m2(v1.m2.m4.m0.m2);
+emit finalResult_v1_m2_m4_m0_m3(v1.m2.m4.m0.m3);
+emit finalResult_v1_m2_m4_m0_m4(v1.m2.m4.m0.m4);
+emit finalResult_v1_m2_m4_m1(v1.m2.m4.m1);
+emit finalResult_v1_m2_m4_m2_m0_m0(v1.m2.m4.m2.m0.m0);
+emit finalResult_v1_m2_m4_m3(v1.m2.m4.m3);
+emit finalResult_v2(v2);
+emit finalResult_v3(v3);
+emit finalResult_v4(v4);
+emit finalResult_v5(v5);
+emit finalResult_v6_m0(v6.m0);
+emit finalResult_v6_m1(v6.m1);
+emit finalResult_v6_m2_m0(v6.m2.m0);
+emit finalResult_v6_m2_m1_m0(v6.m2.m1.m0);
+emit finalResult_v6_m2_m1_m1(v6.m2.m1.m1);
+emit finalResult_v6_m3_m0(v6.m3.m0);
+emit finalResult_v6_m3_m1(v6.m3.m1);
+emit finalResult_v6_m3_m2(v6.m3.m2);
+emit finalResult_v6_m3_m3(v6.m3.m3);
+emit finalResult_v6_m3_m4(v6.m3.m4);
+emit finalResult_v7_m0(v7.m0);
+emit finalResult_v7_m1_m0(v7.m1.m0);
+emit finalResult_v7_m1_m1(v7.m1.m1);
+emit finalResult_v7_m2(v7.m2);
+emit finalResult_v8(v8);
+emit finalResult_v9(v9);
+}
+uint256 _internalMutationsMarkerStorage; uint256 constant _internalMutationsMarkerValue = 0xABABABABABABABABDCDCDCDCDCDCDCDCEAEAEAEAEAEAEAEABCBCBCBCBCBCBCBC;
+uint256 _internalBackupInteger; address _internalBackupAddress; bool _internalBackupBool; string _internalBackupString;
+c0 _internalBackupContractc0; 
+c0.s3 _internalBackupStructs3; c0.s4 _internalBackupStructs4; c0.s5 _internalBackupStructs5; c0.s6 _internalBackupStructs6; c0.s7 _internalBackupStructs7; c0.s8 _internalBackupStructs8; c0.s9 _internalBackupStructs9; c0.s11 _internalBackupStructs11; c0.s10 _internalBackupStructs10; c0.s13 _internalBackupStructs13; c0.s12 _internalBackupStructs12; c0.s0 _internalBackupStructs0; c0.s1 _internalBackupStructs1; c0.s2 _internalBackupStructs2; uint256 _internalBackupSideEffectVariable0; uint256 _internalBackupSideEffectVariable1; uint256 _internalBackupSideEffectVariable2; uint256 _internalBackupSideEffectVariable3; uint256 _internalBackupSideEffectVariable4; uint256 _internalBackupSideEffectVariable5; uint256 _internalBackupSideEffectVariable6; uint256 _internalBackupSideEffectVariable7; uint256 _internalBackupSideEffectVariable8; uint256 _internalBackupSideEffectVariable9; 
+function __annotate(string memory arg) public {}
+}
